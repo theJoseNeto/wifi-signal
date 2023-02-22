fede955126dc1a18c116d1e03e3af2aec96d44f0
theJoseNeto <joseneto180201@gmail.com>
2023-02-22 09:50:03 -0300

    http request working to send wifi signal data to server

--- wifiSignal.ino
+++ wifiSignal.ino
@@ -1,7 +1,9 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const char* wifiUsername = "user";
-const char* wifiPassword = "pass";
+const char* wifiUsername = "<wifi_username>";
+const char* wifiPassword = "<wifi_password>";
+const char* serverName = "<ip_address>:3333/frequence";
 
 void setup()
 {
@@ -12,6 +14,7 @@ void setup()
   delay(10); 
 
   WiFi.begin(wifiUsername, wifiPassword); 
+  
    while (WiFi.status() != WL_CONNECTED) { 
     
     delay(50);
@@ -20,11 +23,35 @@ void setup()
     digitalWrite(2, LOW); 
   
   }  
+
+  
 }
 
   void loop() {  
+    
+    HTTPClient http; 
+    WiFiClient client_; 
 
     const int wifiSignal = WiFi.RSSI(); 
-    // ...  
-   
+    
+    http.begin(client_, serverName); 
+    http.addHeader("Content-Type", "application/json");
+    
+    String data = "{\"rssi\": " + String(wifiSignal) + "}";
+    int httpCode = http.POST(data);
+
+    if (httpCode == HTTP_CODE_OK) {
+      digitalWrite(2, HIGH);
+      delay(20);
+      digitalWrite(2, LOW);
+      delay(20);
+    
+    } 
+
+    if (httpCode != HTTP_CODE_OK) {
+      return;
+    }    
+
+  
+
 }
